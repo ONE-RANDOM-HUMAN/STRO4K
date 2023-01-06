c52691efef6e7ab3b90fcb08252f8da1dcced4e4
ONE-RANDOM-HUMAN <121067143+ONE-RANDOM-HUMAN@users.noreply.github.com>
2023-01-06 00:39:48 +0000

    Implement mobility eval

--- stro/src/evaluate.rs
+++ stro/src/evaluate.rs
@@ -1,3 +1,5 @@
+use crate::consts;
+use crate::movegen::{MoveFn, knight_moves, bishop_moves, rook_moves, queen_moves};
 use crate::position::{Board, Bitboard, Color};
 
 #[repr(C)]
@@ -8,18 +10,32 @@ struct Eval(i16, i16);
 pub const MAX_EVAL: i32 = 256 * 256;
 pub const MIN_EVAL: i32 = -MAX_EVAL;
 
+// Material eval adjusted to average mobility
 const MATERIAL_EVAL: [Eval; 5] = [
     Eval(200, 256),
-    Eval(832, 832),
-    Eval(832, 832),
-    Eval(1344, 1344),
-    Eval(2560, 2560),
+    Eval(832, 832).accum_to(MOBILITY_EVAL[0], -4),
+    Eval(832, 832).accum_to(MOBILITY_EVAL[0], -6),
+    Eval(1344, 1344).accum_to(MOBILITY_EVAL[2], -7),
+    Eval(2560, 2560).accum_to(MOBILITY_EVAL[3], -13),
+];
+
+const MOBILITY_EVAL: [Eval; 4] = [
+    Eval(64, 48),
+    Eval(32, 48),
+    Eval(16, 32),
+    Eval(8, 8),
 ];
 
 impl Eval {
     fn accum(&mut self, eval: Eval, count: i16) {
-        self.0 += eval.0 * count;
-        self.1 += eval.1 * count;
+        *self = self.accum_to(eval, count);
+    }
+
+    const fn accum_to(self, eval: Eval, count: i16) -> Eval {
+        Eval(
+            self.0 + count * eval.0,
+            self.1 + count + eval.1,
+        )
     }
 }
 
@@ -41,6 +57,30 @@ fn resolve(board: &Board, eval: Eval) -> i32 {
     if board.side_to_move() == Color::White { score } else { -score }
 }
 
+fn side_mobility(pieces: &[Bitboard; 6], occ: Bitboard, mask: Bitboard) -> Eval {
+    const MOVE_FNS: [MoveFn; 4] = [
+        knight_moves,
+        bishop_moves,
+        rook_moves,
+        queen_moves,
+    ];
+
+    let mut eval = Eval(0, 0);
+    for i in 0..4 {
+        let mut pieces = pieces[i + 1];
+
+        while pieces != 0 {
+            let piece = pieces & pieces.wrapping_neg();
+            let movement = MOVE_FNS[i](piece, occ) & mask;
+            eval.accum(MOBILITY_EVAL[i], popcnt(movement));
+
+            pieces &= pieces - 1;
+        }
+    }
+
+    eval
+}
+
 pub fn evaluate(board: &Board) -> i32 {
     let mut eval = Eval(0, 0);
     
@@ -49,6 +89,10 @@ pub fn evaluate(board: &Board) -> i32 {
         let count = popcnt(board.pieces()[0][i]) - popcnt(board.pieces()[1][i]);
         eval.accum(MATERIAL_EVAL[i], count);
     }
+
+    let occ = board.white() | board.black();
+    eval.accum(side_mobility(&board.pieces()[0], occ, consts::ALL), 1);
+    eval.accum(side_mobility(&board.pieces()[1], occ, consts::ALL), -1);
 
     resolve(board, eval)
 }
