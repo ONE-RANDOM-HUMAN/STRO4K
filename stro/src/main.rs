f9fbfa29dbe99e1b24a2a877cc300ed530dd891d
ONE-RANDOM-HUMAN <121067143+ONE-RANDOM-HUMAN@users.noreply.github.com>
2023-01-06 00:32:19 +0000

    Fix uci option spelling

--- stro/src/main.rs
+++ stro/src/main.rs
@@ -93,7 +93,7 @@ fn main() {
 
     // Openbench compat
     println!("option name Hash type spin default 0 min 0 max 0");
-    println!("option name Threads type spin defaulet 1 min 1 max 1");
+    println!("option name Threads type spin default 1 min 1 max 1");
 
 
     uci_loop();
