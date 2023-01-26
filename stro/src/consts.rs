b0810437dae711befec426726c30c91c427f9460
ONE-RANDOM-HUMAN <121067143+ONE-RANDOM-HUMAN@users.noreply.github.com>
2023-01-26 23:51:02 +0000

    Implement bishop pair eval
    ELO   | 48.57 +- 23.18 (95%)
    SPRT  | 8.0+0.08s Threads=1 Hash=16MB
    LLR   | 2.99 (-2.94, 2.94) [0.00, 10.00]
    GAMES | N: 648 W: 281 L: 191 D: 176
    
    Bench: 2368738

--- stro/src/consts.rs
+++ stro/src/consts.rs
@@ -5,3 +5,6 @@ pub const ALL: Bitboard = 0xFFFF_FFFF_FFFF_FFFF;
 pub const A_FILE: Bitboard = 0x0101_0101_0101_0101;
 pub const H_FILE: Bitboard = 0x8080_8080_8080_8080;
 pub const AB_FILE: Bitboard = 0x0303_0303_0303_0303;
+
+pub const DARK_SQUARES: Bitboard = 0xAA55_AA55_AA55_AA55;
+pub const LIGHT_SQUARES: Bitboard = 0x55AA_55AA_55AA_55AA;
