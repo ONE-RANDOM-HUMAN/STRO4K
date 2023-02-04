1874a419eda7dce1617e4fcc19008bfa93f69ef1
ONE-RANDOM-HUMAN <121067143+ONE-RANDOM-HUMAN@users.noreply.github.com>
2023-02-04 23:31:52 +0000

    Retune eval
    Bench: 578993

--- stro/src/evaluate.rs
+++ stro/src/evaluate.rs
@@ -11,32 +11,32 @@ pub const MIN_EVAL: i32 = -MAX_EVAL;
 
 // Material eval adjusted to average mobility
 const MATERIAL_EVAL: [Eval; 5] = [
-    Eval(255, 339),
-    Eval(739, 742).accum_to(MOBILITY_EVAL[0], -4),
-    Eval(923, 780).accum_to(MOBILITY_EVAL[1], -6),
-    Eval(1270, 1449).accum_to(MOBILITY_EVAL[2], -7),
-    Eval(2631, 2548).accum_to(MOBILITY_EVAL[3], -13),
+    Eval(249, 346),
+    Eval(732, 735).accum_to(MOBILITY_EVAL[0], -4),
+    Eval(930, 773).accum_to(MOBILITY_EVAL[1], -6),
+    Eval(1264, 1456).accum_to(MOBILITY_EVAL[2], -7),
+    Eval(2663, 2542).accum_to(MOBILITY_EVAL[3], -13),
 ];
 
-const MOBILITY_EVAL: [Eval; 4] = [Eval(34, 23), Eval(26, 23), Eval(26, 20), Eval(18, 16)];
+const MOBILITY_EVAL: [Eval; 4] = [Eval(35, 22), Eval(27, 22), Eval(27, 19), Eval(19, 16)];
 const DOUBLED_PAWN_EVAL: [Eval; 8] = [
-    Eval(-42, -109),
-    Eval( 19,  -62),
-    Eval( 18,  -88),
-    Eval(-29,  -85),
-    Eval(-29,  -85),
-    Eval(-31,  -92),
-    Eval( 16,  -99),
-    Eval(-42, -110),
+    Eval(-77, -87),
+    Eval( 22, -33),
+    Eval(-23, -59),
+    Eval(-74, -80),
+    Eval(-73, -77),
+    Eval(-66, -76),
+    Eval(  8, -79),
+    Eval(-78, -93),
 ];
 
 const PASSED_PAWN_EVAL: [Eval; 6] = [
-    Eval(  4,  54),
-    Eval( 20,  84),
-    Eval( 36, 116),
-    Eval( 57, 172),
-    Eval( 92, 204),
-    Eval(108, 268),
+    Eval(-24,  53),
+    Eval(-27,  14),
+    Eval(-27,  58),
+    Eval( 73, 115),
+    Eval( 93, 125),
+    Eval( 95, 126),
 ];
 
 const BISHOP_PAIR_EVAL: Eval = Eval(128, 128);
