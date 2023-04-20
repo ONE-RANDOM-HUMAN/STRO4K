627066fa8d5aa457afc43fe008b0255c38fe66f8
ONE-RANDOM-HUMAN <121067143+ONE-RANDOM-HUMAN@users.noreply.github.com>
2023-04-20 08:40:55 +0000

    Implement NNUE in STRO
    Bench: 8731304

--- stro/src/evaluate.rs
+++ stro/src/evaluate.rs
@@ -1,6 +1,6 @@
-use crate::consts;
-use crate::movegen::{bishop_moves, knight_moves, queen_moves, rook_moves, MoveFn};
+// use crate::consts;
 use crate::position::{Bitboard, Board, Color};
+use std::arch::x86_64::*;
 
 #[repr(C)]
 #[derive(Clone, Copy, PartialEq, Eq, Default, Debug)]
@@ -9,227 +9,128 @@ struct Eval(i16, i16);
 pub const MAX_EVAL: i32 = 128 * 256 - 1;
 pub const MIN_EVAL: i32 = -MAX_EVAL;
 
-// Material eval adjusted to average mobility
-const MATERIAL_EVAL: [Eval; 5] = [
-    Eval(289, 278),
-    Eval(747, 647).accum_to(MOBILITY_EVAL[0], -4),
-    Eval(866, 679).accum_to(MOBILITY_EVAL[1], -6),
-    Eval(1131, 1197).accum_to(MOBILITY_EVAL[2], -7),
-    Eval(2518, 2119).accum_to(MOBILITY_EVAL[3], -13),
-];
+const NN: [f32; 6333] = unsafe {
+    std::mem::transmute(*include_bytes!("../../nn-13f172599bc152d5.nnue"))
+};
 
-const MOBILITY_EVAL: [Eval; 4] = [Eval(30, 26), Eval(25, 13), Eval(22, 4), Eval(16, 0)];
+const FT_BIAS_OFFSET: usize = 16 * 6 * 64;
+const LAYER_1_WEIGHTS: usize = FT_BIAS_OFFSET + 32;
+const LAYER_1_BIAS: usize = LAYER_1_WEIGHTS + 4 * 32;
+const LAYER_2_WEIGHTS: usize = LAYER_1_BIAS + 4;
+const LAYER_2_BIAS: usize = LAYER_2_WEIGHTS + 4 * 4;
+const LAYER_3_WEIGHTS: usize = LAYER_2_BIAS + 4;
+const LAYER_3_BIAS: usize = LAYER_3_WEIGHTS + 4;
 
-const BISHOP_PAIR_EVAL: Eval = Eval(80, 182);
+const EVAL_SCALE: f32 = 192.0;
 
-#[rustfmt::skip]
-const DOUBLED_PAWN_EVAL: [Eval; 8] = [
-    Eval(-79, -54),
-    Eval(-46, -33),
-    Eval(-64, -26),
-    Eval(-37, -17),
-    Eval(-34, -21),
-    Eval(-43, -39),
-    Eval(-28, -43),
-    Eval(-37, -44),
-];
+fn apply_ft(pieces: &[Bitboard; 6], mask: u32) -> (__m256, __m256) {
+    unsafe {
+        let mut v0 = _mm256_setzero_ps();
+        let mut v1 = _mm256_setzero_ps();
 
-const ISOLATED_PAWN_EVAL: [Eval; 8] = [
-    Eval(-15, -23),
-    Eval(-18, -11),
-    Eval(-41, -24),
-    Eval(-63, -37),
-    Eval(-89, -32),
-    Eval(-44, -30),
-    Eval(-37, -31),
-    Eval(-94, -27),
-];
+        for (i, mut piece) in pieces.iter().copied().enumerate() {
+            while piece != 0 {
+                let square = piece.trailing_zeros() ^ mask;
+                let index = 16 * (i * 64 + square as usize);
+                v0 = _mm256_add_ps(v0, _mm256_loadu_ps(NN.as_ptr().add(index)));
+                v1 = _mm256_add_ps(v1, _mm256_loadu_ps(NN.as_ptr().add(index + 8)));
 
-#[rustfmt::skip]
-const PASSED_PAWN_EVAL: [Eval; 6] = [
-    Eval( 0,    0),
-    Eval( 0,    0),
-    Eval( 0,   23),
-    Eval( 53,  81),
-    Eval(124, 145),
-    Eval(111, 235),
-];
-
-const OPEN_FILE_EVAL: Eval = Eval(83, 3);
-const SEMI_OPEN_FILE_EVAL: Eval = Eval(41, 0);
-
-impl Eval {
-    fn accum(&mut self, eval: Eval, count: i16) {
-        *self = self.accum_to(eval, count);
-    }
-
-    const fn accum_to(self, eval: Eval, count: i16) -> Eval {
-        Eval(self.0 + count * eval.0, self.1 + count * eval.1)
-    }
-}
-
-fn popcnt(bb: Bitboard) -> i16 {
-    bb.count_ones() as i16
-}
-
-fn resolve(board: &Board, eval: Eval) -> i32 {
-    let mut phase: i32 = 0;
-
-    const WEIGHTS: [i32; 4] = [1, 1, 2, 4];
-
-    #[allow(clippy::needless_range_loop)]
-    for i in 0..4 {
-        phase += WEIGHTS[i] * popcnt(board.pieces()[0][i + 1] | board.pieces()[1][i + 1]) as i32;
-    }
-
-    let score = (i32::from(eval.0) * phase + i32::from(eval.1) * (24 - phase)) / 24;
-    if board.side_to_move() == Color::White {
-        score
-    } else {
-        -score
-    }
-}
-
-fn side_mobility(pieces: &[Bitboard; 6], occ: Bitboard, mask: Bitboard) -> Eval {
-    const MOVE_FNS: [MoveFn; 4] = [knight_moves, bishop_moves, rook_moves, queen_moves];
-
-    let mut eval = Eval(0, 0);
-    for i in 0..4 {
-        let mut pieces = pieces[i + 1];
-
-        while pieces != 0 {
-            let piece = pieces & pieces.wrapping_neg();
-            let movement = MOVE_FNS[i](piece, occ) & mask;
-            eval.accum(MOBILITY_EVAL[i], popcnt(movement));
-
-            pieces &= pieces - 1;
-        }
-    }
-
-    eval
-}
-
-fn side_pawn_structure(pawns: Bitboard) -> Eval {
-    let mut eval = Eval(0, 0);
-    let mut file = consts::A_FILE;
-    for i in 0..8 {
-        let pawn_count = popcnt(pawns & file);
-        let adjacent = ((file << 1) & !consts::A_FILE) | ((file & !consts::A_FILE) >> 1);
-        if pawns & adjacent == 0 {
-            eval.accum(ISOLATED_PAWN_EVAL[i], pawn_count);
+                piece &= piece - 1;
+            }
         }
 
-        eval.accum(DOUBLED_PAWN_EVAL[i], pawn_count.max(1) - 1);
-        file <<= 1;
+        (v0, v1)
     }
-
-    eval
-}
-
-// Passed pawns from white's perspective
-fn white_passed_pawn(side: Bitboard, enemy: Bitboard) -> Eval {
-    let mut mask = enemy;
-    mask |= mask >> 8;
-    mask |= mask >> 16;
-    mask |= mask >> 32;
-
-    mask |= ((mask >> 7) & !consts::A_FILE) | ((mask & !consts::A_FILE) >> 9);
-
-    let mut eval = Eval(0, 0);
-    let pawns = side & !mask;
-    let mut file = consts::A_FILE;
-    for _ in 0..8 {
-        let index = (pawns & file).leading_zeros();
-        if index != 64 {
-            eval.accum(PASSED_PAWN_EVAL[(6 - index / 8) as usize], 1);
-        }
-
-        file <<= 1;
-    }
-
-    eval
-}
-
-fn side_open_file(rook: Bitboard, side_pawns: Bitboard, enemy_pawns: Bitboard) -> Eval {
-    let mut eval = Eval(0, 0);
-    let mut file = consts::A_FILE;
-    for _ in 0..8 {
-        if (side_pawns | enemy_pawns) & file == 0 {
-            eval.accum(OPEN_FILE_EVAL, popcnt(rook & file))
-        } else if (side_pawns & file) == 0 {
-            eval.accum(SEMI_OPEN_FILE_EVAL, popcnt(rook & file));
-        }
-
-        file <<= 1;
-    }
-
-    eval
 }
 
 pub fn evaluate(board: &Board) -> i32 {
-    let mut eval = Eval(0, 0);
+    let (v0, v1, v2, v3) = if board.side_to_move() == Color::White {
+        let (v0, v1) = apply_ft(&board.pieces()[0], 0);
+        let (v2, v3) = apply_ft(&board.pieces()[1], 56);
+        (v0, v1, v2, v3)
+    } else {
+        let (v0, v1) = apply_ft(&board.pieces()[1], 56);
+        let (v2, v3) = apply_ft(&board.pieces()[0], 0);
+        (v0, v1, v2, v3)
+    };
 
-    // material
-    #[allow(clippy::needless_range_loop)]
-    for i in 0..5 {
-        let count = popcnt(board.pieces()[0][i]) - popcnt(board.pieces()[1][i]);
-        eval.accum(MATERIAL_EVAL[i], count);
+    unsafe {
+        let v0 = _mm256_add_ps(v0, _mm256_loadu_ps(NN.as_ptr().add(FT_BIAS_OFFSET)));
+        let v1 = _mm256_add_ps(v1, _mm256_loadu_ps(NN.as_ptr().add(FT_BIAS_OFFSET + 8)));
+        let v2 = _mm256_add_ps(v2, _mm256_loadu_ps(NN.as_ptr().add(FT_BIAS_OFFSET + 16)));
+        let v3 = _mm256_add_ps(v3, _mm256_loadu_ps(NN.as_ptr().add(FT_BIAS_OFFSET + 24)));
+
+        // relu
+        let v0 = _mm256_max_ps(v0, _mm256_setzero_ps());
+        let v1 = _mm256_max_ps(v1, _mm256_setzero_ps());
+        let v2 = _mm256_max_ps(v2, _mm256_setzero_ps());
+        let v3 = _mm256_max_ps(v3, _mm256_setzero_ps());
+
+
+        let mut a0 = _mm256_setzero_ps();
+        let mut a1 = _mm256_setzero_ps();
+        let mut a2 = _mm256_setzero_ps();
+        let mut a3 = _mm256_setzero_ps();
+
+        let mut perm = _mm256_set1_epi32(0b11100100);
+
+        // let v0
+        for i in 0..4 {
+            a0 = _mm256_fmadd_ps(
+                _mm256_permutevar_ps(v0, perm),
+                _mm256_loadu_ps(NN.as_ptr().add(LAYER_1_WEIGHTS + i * 32)),
+                a0,
+            );
+
+            a1 = _mm256_fmadd_ps(
+                _mm256_permutevar_ps(v1, perm),
+                _mm256_loadu_ps(NN.as_ptr().add(LAYER_1_WEIGHTS + i * 32 + 8)),
+                a1,
+            );
+
+            a2 = _mm256_fmadd_ps(
+                _mm256_permutevar_ps(v2, perm),
+                _mm256_loadu_ps(NN.as_ptr().add(LAYER_1_WEIGHTS + i * 32 + 16)),
+                a2,
+            );
+
+            a3 = _mm256_fmadd_ps(
+                _mm256_permutevar_ps(v3, perm),
+                _mm256_loadu_ps(NN.as_ptr().add(LAYER_1_WEIGHTS + i * 32 + 24)),
+                a3,
+            );
+
+            perm = _mm256_srli_epi32::<2>(perm);
+        }
+
+        let a0 = _mm256_add_ps(a0, a1);
+        let a1 = _mm256_add_ps(a2, a3);
+        let acc = _mm256_add_ps(a0, a1);
+        let acc = _mm_add_ps(
+            _mm256_castps256_ps128(acc),
+            _mm256_extractf128_ps(acc, 1)
+        );
+
+        let acc = _mm_add_ps(acc, _mm_loadu_ps(NN.as_ptr().add(LAYER_1_BIAS)));
+        let acc = _mm_max_ps(acc, _mm_setzero_ps());
+
+        let a0 = _mm_dp_ps::<0b11110001>(acc, _mm_loadu_ps(NN.as_ptr().add(LAYER_2_WEIGHTS)));
+        let a1 = _mm_dp_ps::<0b11110010>(acc, _mm_loadu_ps(NN.as_ptr().add(LAYER_2_WEIGHTS + 4)));
+        let a2 = _mm_dp_ps::<0b11110100>(acc, _mm_loadu_ps(NN.as_ptr().add(LAYER_2_WEIGHTS + 8)));
+        let a3 = _mm_dp_ps::<0b11111000>(acc, _mm_loadu_ps(NN.as_ptr().add(LAYER_2_WEIGHTS + 12)));
+
+        let acc = _mm_or_ps(
+            _mm_or_ps(a0, a1),
+            _mm_or_ps(a2, a3),
+        );
+
+    
+        let acc = _mm_add_ps(acc, _mm_loadu_ps(NN.as_ptr().add(LAYER_2_BIAS)));
+        let acc = _mm_max_ps(acc, _mm_setzero_ps());
+
+        let acc = _mm_dp_ps::<0b11110001>(acc, _mm_loadu_ps(NN.as_ptr().add(LAYER_3_WEIGHTS)));
+        let eval = _mm_cvtss_f32(acc);
+
+        ((eval + NN[LAYER_3_BIAS]) * EVAL_SCALE) as i32
     }
-
-    // bishop pair
-    if board.pieces()[0][2] & consts::DARK_SQUARES != 0
-        && board.pieces()[0][2] & consts::LIGHT_SQUARES != 0
-    {
-        eval.accum(BISHOP_PAIR_EVAL, 1);
-    }
-
-    if board.pieces()[1][2] & consts::DARK_SQUARES != 0
-        && board.pieces()[1][2] & consts::LIGHT_SQUARES != 0
-    {
-        eval.accum(BISHOP_PAIR_EVAL, -1);
-    }
-
-    // mobility
-    let occ = board.white() | board.black();
-    eval.accum(side_mobility(&board.pieces()[0], occ, consts::ALL), 1);
-    eval.accum(side_mobility(&board.pieces()[1], occ, consts::ALL), -1);
-
-    // doubled pawns
-    eval.accum(side_pawn_structure(board.pieces()[0][0]), 1);
-    eval.accum(side_pawn_structure(board.pieces()[1][0]), -1);
-
-    // passed pawns
-    eval.accum(
-        white_passed_pawn(board.pieces()[0][0], board.pieces()[1][0]),
-        1,
-    );
-
-    eval.accum(
-        white_passed_pawn(
-            board.pieces()[1][0].swap_bytes(),
-            board.pieces()[0][0].swap_bytes(),
-        ),
-        -1,
-    );
-
-    // open files
-    eval.accum(
-        side_open_file(
-            board.pieces()[0][3],
-            board.pieces()[0][0],
-            board.pieces()[1][0],
-        ),
-        1,
-    );
-
-    eval.accum(
-        side_open_file(
-            board.pieces()[1][3],
-            board.pieces()[1][0],
-            board.pieces()[0][0],
-        ),
-        -1,
-    );
-
-    resolve(board, eval)
 }
