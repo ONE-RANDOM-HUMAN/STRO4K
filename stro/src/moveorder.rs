39cca84a42ae5a4a4a6b05d07d4a76ef04f6d6ee
ONE-RANDOM-HUMAN <121067143+ONE-RANDOM-HUMAN@users.noreply.github.com>
2024-12-16 23:51:28 +0000

    Increase weighting of cmh
    Size: 4043
    
    Bench: 749156

--- stro/src/moveorder.rs
+++ stro/src/moveorder.rs
@@ -85,7 +85,7 @@ pub fn order_noisy_moves(position: &Board, moves: &mut [MovePlus]) -> usize {
 
 pub fn order_quiet_moves(moves: &mut [MovePlus], kt: KillerTable, history: &HistoryTable, cmh: &HistoryTable) -> usize {
     for mov in &mut *moves {
-        mov.score = history.get(mov.mov) + cmh.get(mov.mov);
+        mov.score = history.get(mov.mov) + 2 * cmh.get(mov.mov);
 
         // killers
         if let Some(index) = kt.index(mov.mov) {
