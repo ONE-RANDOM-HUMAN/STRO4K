15e6aee64807f323a9e20c9c1c69a8323ec541f4
ONE-RANDOM-HUMAN <121067143+ONE-RANDOM-HUMAN@users.noreply.github.com>
2025-10-23 15:44:09 +0000

    Implement material corrhist in STRO and use smaller hash
    Elo   | 17.79 +- 9.38 (95%)
    SPRT  | 8.0+0.08s Threads=1 Hash=32MB
    LLR   | 2.90 (-2.25, 2.89) [0.00, 5.00]
    Games | N: 3166 W: 1031 L: 869 D: 1266
    Penta | [81, 365, 593, 399, 145]
    
    Elo   | 25.41 +- 11.43 (95%)
    SPRT  | 40.0+0.40s Threads=1 Hash=128MB
    LLR   | 2.90 (-2.25, 2.89) [0.00, 5.00]
    Games | N: 2000 W: 634 L: 488 D: 878
    Penta | [38, 199, 413, 279, 71]
    
    Size: 4057
    
    Bench: 514417

--- stro/src/position.rs
+++ stro/src/position.rs
@@ -332,16 +332,33 @@ impl Board {
         unsafe {
             use std::arch::x86_64::*;
 
-            let mut value = _mm_cvtsi32_si128(
-                self.side_to_move as i32 | i32::from(self.ep) << 8 | i32::from(self.castling) << 16,
-            );
-
             let ptr = self as *const _ as *const i64;
+            let mut value =
+                _mm_loadu_si128((&raw const self.fifty_moves).cast::<__m128i>().offset(-1));
 
             // pieces and color
             for i in (0..=12).rev() {
                 value = _mm_aesenc_si128(value, _mm_loadu_si128(ptr.add(i).cast()));
             }
+
+            _mm_cvtsi128_si64x(value) as u64
+        }
+    }
+
+    pub fn material_hash(&self) -> u64 {
+        // SAFETY: aes-ni and 64 bit required for build
+        unsafe {
+            use std::arch::x86_64::*;
+
+            let mut data = [0; 16];
+            for (i, bb) in self.pieces.iter().flatten().enumerate() {
+                data[i] = bb.count_ones() as u8;
+            }
+
+            let mut value = _mm_loadu_si128(data.as_ptr().cast());
+            value = _mm_aesenc_si128(value, value);
+            value = _mm_aesenc_si128(value, value);
+            value = _mm_aesenc_si128(value, value);
 
             _mm_cvtsi128_si64x(value) as u64
         }
