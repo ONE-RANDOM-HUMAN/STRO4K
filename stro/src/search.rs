3536f038446d4a415571855e3a31cd09bb090e2b
ONE-RANDOM-HUMAN <121067143+ONE-RANDOM-HUMAN@users.noreply.github.com>
2025-10-27 14:44:34 +0000

    Decrease corrhist scale
    Size: 4057
    
    Bench: 523229

--- stro/src/search.rs
+++ stro/src/search.rs
@@ -12,7 +12,7 @@ use crate::position::{Board, Move};
 use crate::tt::{self, Bound, TTData};
 
 const CORR_HIST_ENTRIES: usize = 1 << 16;
-const CORR_HIST_SCALE_SHIFT: u32 = 9;
+const CORR_HIST_SCALE_SHIFT: u32 = 8;
 const CORR_HIST_SCALE: i32 = 1 << CORR_HIST_SCALE_SHIFT;
 const CORR_HIST_MAX_WEIGHT: i32 = 1 << 5;
 const CORR_HIST_MAX: i32 = 96;
