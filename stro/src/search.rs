e06c821a221c3acef1e7a838169bf2b129f033f7
ONE-RANDOM-HUMAN <121067143+ONE-RANDOM-HUMAN@users.noreply.github.com>
2026-06-07 12:31:40 +0000

    Increase corrhist scale and max weight
    Size: 4079
    
    Bench: 970702

--- stro/src/search.rs
+++ stro/src/search.rs
@@ -12,9 +12,9 @@ use crate::position::{Board, Color, Move};
 use crate::tt::{self, Bound, TTData};
 
 const CORR_HIST_ENTRIES: usize = 1 << 16;
-const CORR_HIST_SCALE_SHIFT: u32 = 9;
+const CORR_HIST_SCALE_SHIFT: u32 = 11;
 const CORR_HIST_SCALE: i32 = 1 << CORR_HIST_SCALE_SHIFT;
-const CORR_HIST_MAX_WEIGHT: i32 = 1 << 5;
+const CORR_HIST_MAX_WEIGHT: i32 = 128;
 const CORR_HIST_MAX: i32 = 128;
 
 #[unsafe(no_mangle)]
